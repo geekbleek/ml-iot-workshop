6a8cabdd0015ad426ba1db6a74d904fdfc9f0cb1
cbleeker <casey@geekbleek.com>
2018-08-21 18:52:15 -0700

    Update python script

--- receive.py
+++ receive.py
@@ -18,13 +18,16 @@ def on_message(client,userdata,message):
         jsonData = json.dumps(messagesReceived)
         data = pd.read_json(jsonData)
         data.to_csv('./sensor_data.csv', index=False, columns = ['weight', 'humidity', 'temperature', 'prod_id'])
-        print("Data captured to disk")
+        print("Data captured to disk.  You can exit the application with Ctrl/Cmd+C.")
 
 mqttc = mqtt.Client()
 
 mqttc.on_message = on_message 
 
-mqttc.connect("brokerUrl.domain.com") #Change to mqtt.cisco.com or 128.107.70.30 to access Cisco's test/public MQTT broker.
+mqttc.connect("Enter broker URL here.") #Change to mqtt.cisco.com or 128.107.70.30 to access Cisco's test/public MQTT broker.
+
+mqttc.on_connect = print('Connected to MQTT Broker.')
+
 mqttc.subscribe("devnet/sensors")
 
 mqttc.loop_forever() #start the loop
